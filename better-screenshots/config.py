bcd5de3ec9bea1e64ee45b585f542fa4117ff3a2
Greg <gregloj2001@gmail.com>
2024-05-30 17:18:05 -0400

    Change comments in config.py

---
+++ better-screenshots/config.py
@@ -0,0 +1,19 @@
+# Enter your Steam API key here. You can access you API key here: https://steamcommunity.com/dev/apikey
+
+steam_key = ""
+
+# Enter the path of your uncompressed screenshots folder here.
+# Example: C:\Users\jsmith\Documents\External-Screenshots
+
+# NOTE: If not done so yet, enable External Screenshots from your Steam application:
+# Steam -> Settings -> In Game -> Screenshots -> Tick "Save an external copy of my screenshots"
+# -> Choose the folder you want for your path
+
+# NOTE: Make sure the path in your steam settings and the path you fill below are identical. 
+
+screenshot_path = r""
+
+
+
+
+
