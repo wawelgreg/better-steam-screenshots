8b31da32fdedaabb8836ee8240ea047b177ac91c
Greg <gregloj2001@gmail.com>
2024-05-31 18:07:54 -0400

    Start implementation of image sorting and movement

--- better-screenshots/file_sorter.py
+++ better-screenshots/file_sorter.py
@@ -1,3 +1,4 @@
+import os
 import steam_api_setup as sas
 import config as conf
 import json
@@ -11,10 +12,20 @@ log.basicConfig(filename='bss.log', format=FORMAT, level=log.INFO)
 log.info("Attaining steam api with config key...")
 steam = sas.steam_key()
 log.info("Success!")
+log.info("Attaining root and destination screenshot folder...")
+ROOT = Path(conf.screenshot_path)
+DEST = Path(conf.destination_path)
+log.info("Success!")
+
+
+def path_exist(P):
+    if (P.is_file()):
+        return True
+    return False
 
 
 def import_local_dict():
-    if (not Path(r"better-screenshots/game_list_data.json").is_file()):
+    if (not path_exist(Path(r"better-screenshots/game_list_data.json"))):
         log.info("Game list data DNE. Returning empty dict.")
         return dict()
     
@@ -29,6 +40,18 @@ def search_game_name(game_id):
     return steam.apps.get_app_details(game_id)[str(game_id)]["data"]["name"]
 
 
+def image_iter(local_game_dict):
+    for file in ROOT.glob("*.png"):
+        sort_store_image(file, local_game_dict)
+
+
+def sort_store_image(file, local_game_dict):
+    if (not path_exist(DEST)):
+        DEST.mkdir(parents=True, exist_ok=True)
+    pass
+
+
 if __name__ == "__main__":
     log.info("Attaining local game list data.")
     local_game_dict = import_local_dict()
+    image_iter(local_game_dict)
