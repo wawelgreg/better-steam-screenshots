32fde1830fe80a058501071af7600cf9c81c5d46
Greg <gregloj2001@gmail.com>
2024-05-31 16:53:58 -0400

    Create import_local_dict()

--- better-screenshots/file_sorter.py
+++ better-screenshots/file_sorter.py
@@ -12,3 +12,18 @@ log.info("Attaining steam api with config key...")
 steam = sas.steam_key()
 log.info("Success!")
 
+def import_local_dict():
+    if (not Path(r"better-screenshots/game_list_data.json").is_file()):
+        log.info("Game list data DNE. Returning empty dict.")
+        return dict()
+    
+    log.info("game_list_data.json exists -> converting to dict.")
+    with open(r"better-screenshots/game_list_data.json") as json_file:
+        lgd = json.load(json_file)
+    log.info("Returning dict.")
+    return lgd
+
+if __name__ == "__main__":
+    log.info("Attaining local game list data.")
+    local_game_dict = import_local_dict()
+    print(local_game_dict)
