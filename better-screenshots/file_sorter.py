d9cd761100bae540df8284ab6d3dee3bce3fd6fb
Greg <gregloj2001@gmail.com>
2024-06-02 12:49:14 -0400

    Implement file information parsing

--- better-screenshots/file_sorter.py
+++ better-screenshots/file_sorter.py
@@ -5,6 +5,7 @@ import json
 import logging as log
 from pathlib import Path
 import shutil
+import string
 
 FORMAT = '%(asctime)s %(message)s'
 log.basicConfig(filename='bss.log', format=FORMAT, level=log.INFO)
@@ -18,14 +19,8 @@ DEST = Path(conf.destination_path)
 log.info("Success!")
 
 
-def path_exist(P):
-    if (P.is_file()):
-        return True
-    return False
-
-
 def import_local_dict():
-    if (not path_exist(Path(r"better-screenshots/game_list_data.json"))):
+    if (not Path(r"better-screenshots/game_list_data.json").exists()):
         log.info("Game list data DNE. Returning empty dict.")
         return dict()
     
@@ -36,19 +31,43 @@ def import_local_dict():
     return lgd
 
 
-def search_game_name(game_id):
+def update_local_dict(local_game_dict: dict, game_id: int, game_name: str):
+    local_game_dict[game_id] = game_name
+
+
+def search_game_name(game_id: int):
     return steam.apps.get_app_details(game_id)[str(game_id)]["data"]["name"]
 
 
-def image_iter(local_game_dict):
+def image_iter(local_game_dict: dict):
     for file in ROOT.glob("*.png"):
         sort_store_image(file, local_game_dict)
 
-
 def sort_store_image(file, local_game_dict):
-    if (not path_exist(DEST)):
+    if (not DEST.exists()):
         DEST.mkdir(parents=True, exist_ok=True)
-    pass
+    
+    details = file.name.removesuffix(".png")
+    parsed = details.split('_')
+
+    log.info("Checking game name existence in dict: {}".format(local_game_dict))
+    if parsed[0] in local_game_dict:
+        scrn_game_name = local_game_dict[parsed[0]]
+    else:
+        scrn_game_name = search_game_name(parsed[0])
+        update_local_dict(local_game_dict, parsed[0], scrn_game_name)
+        log.info("New game name: local dict: {}".format(local_game_dict))
+
+    scrn_year = parsed[1][0:4]
+    scrn_month = parsed[1][4:6]
+    scrn_day = parsed[1][6:8]
+    scrn_hour = parsed[1][8:10]
+    scrn_minute = parsed[1][10:12]
+    scrn_seconds = parsed[1][12:]
+
+    log.info("%s %s %s %s %s:%s:%s", 
+             scrn_game_name, scrn_year, scrn_month,
+             scrn_day, scrn_hour, scrn_minute, scrn_seconds)
 
 
 if __name__ == "__main__":
