151354bcbf85dbe83cb25173e61a957b9032f865
Greg <gregloj2001@gmail.com>
2024-06-05 17:31:08 -0400

    Change dependency fron old config to data.json

--- better-screenshots/steam_api_setup.py
+++ better-screenshots/steam_api_setup.py
@@ -1,8 +1,8 @@
-import config as conf
+import file_sorter as fs
 import os
 from steam_web_api import Steam
 
 def steam_key():
-    KEY = os.environ.get(conf.steam_key)
+    KEY = os.environ.get(fs.import_data_dict_json()["Key"])
     steam = Steam(KEY)
     return steam
