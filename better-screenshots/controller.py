0079f18cd8387cbaf6c54ece6e828409e0a016d7
Greg <gregloj2001@gmail.com>
2024-06-11 12:40:45 -0400

    Call file_sorter.sort() with passed tuple of entry values

--- better-screenshots/controller.py
+++ better-screenshots/controller.py
@@ -6,10 +6,16 @@ def save_settings(key_text: str, src_text: str, dest_text: str):
     res_dict = {"Key": key_text, "Src": src_text, "Dest": dest_text}
     fs.update_dict_json(res_dict, fs.DEFAULT_DATA_JSON_PATH)
 
+
 # Get saved key, src, and dest from file_sorter
 def get_settings() -> dict:
     return fs.import_data_dict_json()
 
+
 # Run sorting algorithm in file_sorter
-def run_sort():
-    fs.sort()
+# def run_sort():
+#     fs.sort()
+
+# Run sorting algorithm in file_sorter with data from entry widgets
+def run_sort(entry_values: tuple):
+    fs.sort(entry_values)
