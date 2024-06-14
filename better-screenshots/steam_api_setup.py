4fa9478ceb011f2833fb5414c1331b893ade4cb8
Greg <gregloj2001@gmail.com>
2024-06-13 21:46:37 -0400

    Fix steam_access()

--- better-screenshots/steam_api_setup.py
+++ better-screenshots/steam_api_setup.py
@@ -2,6 +2,5 @@ import os
 from steam_web_api import Steam
 
 def steam_access(key_text: str) -> Steam:
-    KEY = os.environ.get(key_text)
-    steam = Steam(KEY)
+    steam = Steam(key_text)
     return steam
