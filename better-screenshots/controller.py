cb06e6cfc4ff599c1673e3345d87378f7978b9f4
Greg <gregloj2001@gmail.com>
2024-06-13 22:13:03 -0400

    Fix comments with docstring conventions

--- better-screenshots/controller.py
+++ better-screenshots/controller.py
@@ -1,17 +1,18 @@
 import file_sorter as fs
 from log_manager import log
 
-# Get values from gui and send to file_sorter
+
 def save_settings(key_text: str, src_text: str, dest_text: str):
+    '''Get values from GUI and send to file_sorter'''
     res_dict = {"Key": key_text, "Src": src_text, "Dest": dest_text}
     fs.update_dict_json(res_dict, fs.DEFAULT_DATA_JSON_PATH)
 
 
-# Get saved key, src, and dest from file_sorter
 def get_settings() -> dict:
+    '''Get saved key, src, and dest from file_sorter'''
     return fs.import_data_dict_json()
 
 
-# Run sorting algorithm in file_sorter with data from entry widgets
 def run_sort(entry_values: tuple):
+    '''Run sorting algotithm in file_sorter with data from entry widgets'''
     fs.sort(entry_values)
